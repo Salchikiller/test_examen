0b125e3b00651fecd4fe4068dee16362432a7265
Your Name <you@example.com>
2024-05-26 02:22:56 +0200

    Test 2

--- examen/test/test_serv.py
+++ examen/test/test_serv.py
@@ -7,6 +7,14 @@ class TestServer(unittest.TestCase):
     def test_fecha(self):
         response = server.generate_response('FECHA')
         self.assertRegex(response, r'\d{4}-\d{2}-\d{2}')
+
+    def test_hora(self):
+        # Forcing the test to fail
+        response = server.generate_response('HORA')
+        self.assertNotRegex(response, r'\d{2}:\d{2}:\d{2}')
+
+
+
     
 if __name__ == '__main__':
     unittest.main()
